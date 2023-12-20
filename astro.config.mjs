b51d8be5662e34ffd9c201eae51b116437f581e8
Yae <106218352+Smallyae@users.noreply.github.com>
2023-12-19 22:27:52 -0500

    Pagina participantes terminada

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({
-  site: "https://smallyae.github.io",
-  base: "/Showmaker-Game-Jam",
-});
+export default defineConfig({});
